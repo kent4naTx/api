7e72725afabd9344bbb4b934159144f0d11ffc21
Lucas <lucas.81.gabriel@gmail.com>
2023-11-16 22:15:40 -0300

    ✨ feat: migrations de cidade, estado e endereco

---
+++ database/migrations/2023_11_17_010240_create_endereco_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('endereco', function (Blueprint $table) {
+            $table->id();
+            $table->string('cep', 10);
+            $table->string('bairro', 80);
+            $table->string('rua', 80);
+            $table->string('numero', 6);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('endereco');
+    }
+};
