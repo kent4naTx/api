dbc6d3d1b6a490904555eadcab8dbd7423e536d5
Lucas <lucas.81.gabriel@gmail.com>
2023-11-17 22:59:08 -0300

    ✨ feat: usuario rest controller

--- app/Http/Controllers/Usuario/UsuarioController.php
+++ app/Http/Controllers/Usuario/UsuarioController.php
@@ -3,9 +3,89 @@
 namespace App\Http\Controllers\Usuario;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Usuario\CriarUsuarioRequest;
+use App\Http\Requests\Usuario\AtualizarUsuarioRequest;
+use App\Models\Usuario\Usuario;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class UsuarioController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        return parent::apiResponse(200, true, "Dados recuperados com sucesso", Usuario::all());
+    }
+
+    public function show(int $id): JsonResponse
+    {
+        $usuario = Usuario::find($id);
+        if (is_null($usuario)) {
+
+            return parent::apiResponse(201, false, "Usuario não existe");
+        }
+
+        return parent::apiResponse(200, true, "Dados recuperados com sucesso", $usuario);
+    }
+
+    public function store(CriarUsuarioRequest $request): JsonResponse
+    {
+        try {
+            DB::beginTransaction();
+            $usuario = Usuario::create($request->validated());
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            return parent::apiResponse(201, false, "Não foi possível criar usuario", $e);
+        }
+
+        return parent::apiResponse(200, true, "Dados Criados com sucesso", $usuario);
+    }
+
+    public function update(AtualizarUsuarioRequest $request, int $id): JsonResponse
+    {
+
+        $usuario = Usuario::find($id);
+        if (is_null($usuario)) {
+
+            return parent::apiResponse(201, false, "Usuario não foi encontrado");
+        }
+
+        try {
+            DB::beginTransaction();
+            foreach ($request->validated() as $key => $value) {
+                $usuario->$key = $value;
+            }
+            $usuario->save();
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollback();
+            
+            return parent::apiResponse(201, false, "Usuario não pode ser atualizado", $e);
+        }
+
+        return parent::apiResponse(200, true, "Dados foram atualizados com sucesso", $usuario);
+    }
+
+    public function destroy(int $id): JsonResponse
+    {
+
+        $usuario = Usuario::find($id);
+        if (is_null($usuario)) {
+
+            return parent::apiResponse(201, false, "Usuario não foi encontrado");
+        }
+
+        try {
+            DB::beginTransaction();
+            $usuario->destroy();
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            return parent::apiResponse(201, false, "Usuario não pode ser deletado", $e);
+        }
+        return parent::apiResponse(200, true, "Usuario foi deleteado com sucesso", $usuario);
+    }
 }
