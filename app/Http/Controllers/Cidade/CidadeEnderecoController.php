898d266b85887385ee55b377afcc74a9b41797c0
Lucas <lucas.81.gabriel@gmail.com>
2023-11-17 21:28:44 -0300

    ✨ feat: cidade, documento, endereco

---
+++ app/Http/Controllers/Cidade/CidadeEnderecoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Cidade;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CidadeEnderecoController extends Controller
+{
+    //
+}
